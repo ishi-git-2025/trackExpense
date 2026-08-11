ff6f012c4f4072cf5f9be8f6eb7282c08f757f81
Ishita Agrawal <ishita.agarwal@geminisolutions.com>
2026-08-11 12:43:24 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express(); 
+const port = 4000; 
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Expense Tracker API");
+});
+
+app.listen(port, () => { // this is where the server starts listening for incoming requests
+  console.log(`Server is running on port ${port}`);
+});
